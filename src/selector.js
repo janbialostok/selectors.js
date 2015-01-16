8cf660297b33e6a1cff19ede75da7a3ebc222bd4
Jan Bialostok <janbialostok@gmail.com>
2015-01-16 11:34:30 -0500

    First ten test specs passed

--- src/selector.js
+++ src/selector.js
@@ -17,25 +17,99 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 // return one of these types: id, class, tag.class, tag
 //
 var selectorTypeMatcher = function(selector) {
+  var splitSelector = selector.split("");
+  if (splitSelector[0] == "#"){
+    return "id";
+  }
+  else if (splitSelector[0] == "."){
+    return "class";
+  }
+  else{
+    var selectSplit = selector.split(".");
+    if (selectSplit.length == 2){
+      return "tag.class";
+    }
+    else if (selectSplit.length > 2){
+      return "Invalid Class";
+    }
+    else{
+      return "tag";
+    }
+  }
+
   // your code here
 };
 
 var matchFunctionMaker = function(selector) {
+  var forTagNames = selector;
+  var splitSelector = selector.split("");
+  splitSelector.shift();
+  splitSelector = splitSelector.join("");
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") {
-    // define matchFunction for id
+      if (selectorType === "id") {
+        var matchFunction = function(element){
+            if (element.getAttribute('id').toString() == splitSelector){
+              return true;
+            }
+            else{
+              return false;
+            }
+          /*console.log(selector);
+          
+          var allNodes = document.body.children;
+          //console.log(document.body.children);
+          for(var i = 0; i < document.body.children.length; i++) {
+            if (allNodes[i].hasAttribute('id')){
+                console.log(allNodes[i].getAttribute('id'))
+                if (allNodes[i].getAttribute('id') == cleanSelector){
+                  return true;
+                }
+            
+            }
+          }
+          return false;*/
+        }
+        // define matchFunction for id
+      } else if (selectorType === "class") {
+        // define matchFunction for class
+        matchFunction = function(element){
+          var classes = element.getAttribute('class').split(" ");
+          for (var i = 0; i < classes.length; i++){
+            if (classes[i] == splitSelector){
+              return true;
+            }
+          }
+          return false;
+        }
+        
+      } else if (selectorType === "tag.class") {
+        // define matchFunction for tag.class
+        var split_tag = forTagNames.split(".");
+        matchFunction = function(element){
+          var classes = element.getAttribute('class').split(" ");  
+          for (var i = 0; i < classes.length; i++){
+            if (classes[i] == split_tag[1] && element.tagName.toString().toLowerCase() == split_tag[0]){
+              return true;
+            }
+          }
+          return false;
+        }
+        
+      } else if (selectorType === "tag") {
+        // define matchFunction for tag
+        matchFunction = function(element){
+          if (element.tagName.toString().toLowerCase() == forTagNames){
+              return true;
+            }
+            else{
+              return false;
+            }
+          
+        }
 
-  } else if (selectorType === "class") {
-    // define matchFunction for class
-    
-  } else if (selectorType === "tag.class") {
-    // define matchFunction for tag.class
-    
-  } else if (selectorType === "tag") {
-    // define matchFunction for tag
-    
   }
+
   return matchFunction;
 };
 
